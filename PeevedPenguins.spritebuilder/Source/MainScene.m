9eca4aa947950f72264d416e91d9fe49edbaaf24
Bartlomiej Pater <nostah@icloud.com>
2014-11-01 15:30:45 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -11,6 +11,8 @@
 @implementation MainScene
 
 - (void)play {
-    CCLOG(@"play pressed");
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
+
 @end
