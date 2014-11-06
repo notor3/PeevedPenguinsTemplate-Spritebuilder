66689140c077ded551222e395a1d8fde31bfd725
Bartlomiej Pater <nostah@icloud.com>
2014-11-07 00:10:05 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Bartłomiej Pater on 07.11.2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,36 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Bartłomiej Pater on 07.11.2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay {
+
+CCPhysicsNode *_physicsnode;
+CCNode *_catapultarm;
+
+}
+
+- (void)didLoadFromCCB {
+    self.userInteractionEnabled = YES;
+}
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self launchPenguin];
+}
+
+- (void)launchPenguin {
+    CCNode *penguin = [CCBReader load:@"Penguin"];
+    penguin.position = ccpAdd(_catapultarm.position, ccp(16, 50));
+    [_physicsnode addChild:penguin];
+    
+    CGPoint direction = ccp(1, 0);
+    CGPoint force = ccpMult(direction, 8000);
+    [penguin.physicsBody applyForce:force];
+}
+
+@end
