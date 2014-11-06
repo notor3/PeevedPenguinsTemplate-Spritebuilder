83ef4d3baab68a0a193efdae20464d2df7c0b202
Bartlomiej Pater <nostah@icloud.com>
2014-11-07 00:10:53 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -25,7 +25,7 @@ CCNode *_catapultarm;
 
 - (void)launchPenguin {
     CCNode *penguin = [CCBReader load:@"Penguin"];
-    penguin.position = ccpAdd(_catapultarm.position, ccp(16, 100));
+    penguin.position = ccpAdd(_catapultarm.position, ccp(16, 120));
     [_physicsnode addChild:penguin];
     
     CGPoint direction = ccp(1, 0);
