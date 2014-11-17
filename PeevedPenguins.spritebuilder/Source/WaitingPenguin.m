d6882cf6856fd7ed244589a3d62ef6cc72387671
Bartlomiej Pater <nostah@icloud.com>
2014-11-17 01:03:20 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -0,0 +1,23 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by Bartłomiej Pater on 17/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+-(void) didLoadFromCCB {
+    float delay = (arc4random() % 2000) / 1000.f;
+    [self performSelector:startPenguin withObject:nil afterDelay:delay];
+}
+
+- (void)startPenguin {
+    CCAnimationManager *am = self.animationManager;
+    [am runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
+
+@end
